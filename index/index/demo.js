b1a0af72850b5550095e7df935b5e01cf01caf7b
anubhardwaj2193 <anubhardwaj2193>
2023-02-14 14:54:12 +0530

    third commit

---
+++ index/index/demo.js
@@ -0,0 +1,8 @@
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[2]);
+items[2].style.backgroundColor = 'green';
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight='bold';
+}
