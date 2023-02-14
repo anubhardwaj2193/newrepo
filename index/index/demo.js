d89592a15dc0a0473492286fe467dcad935b1150
anubhardwaj2193 <anubhardwaj2193>
2023-02-14 18:19:35 +0530

    fourth commit

--- index/index/demo.js
+++ index/index/demo.js
@@ -1,8 +1,8 @@
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[2]);
-items[2].style.backgroundColor = 'green';
-for(var i=0;i<items.length;i++)
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[2]);
+li[2].style.backgroundColor = 'green';
+for(var i=0;i<li.length;i++)
 {
-    items[i].style.fontWeight='bold';
+    li[i].style.fontWeight='bold';
 }
